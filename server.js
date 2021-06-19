2aa0f4b908cf63dcff9058c6315a240a0e914901
kkxxll <550893685@qq.com>
2021-06-19 12:43:24 +0800

    解决路由映射后刷新会404错误

--- server.js
+++ server.js
@@ -27,6 +27,18 @@ const router = new Router();
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
+
+  router.get("/a/:id", async (ctx) => {
+    const id = ctx.params.id;
+    await handle(ctx.req, ctx.res, {
+      pathname: "/a",
+      query: { id },
+    });
+    ctx.respond = false;
+  });
+
+  server.use(router.routes())
+
   server.use(async (ctx, next) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
