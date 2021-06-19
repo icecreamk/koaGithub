1489d6a14cc08b262445b0868bd4c6d17c55cb29
kkxxll <550893685@qq.com>
2021-06-19 11:41:10 +0800

    请求路由

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
@@ -6,13 +7,31 @@ const app = next({ dev });
 
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = new Koa();
-  server.use(async (ctx, next) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-  });
-  server.listen(3000, () => {
-    console.log("koa server listening on 3000");
-  });
+const server = new Koa();
+const router = new Router();
+
+router.get('/test', (ctx) => {
+  ctx.body = 'test'
+})
+
+server.use(async (ctx, next) => {
+  ctx.body = "INDEX";
+  await next();
 });
+
+server.use(router.routes())
+
+server.listen(3000, () => {
+  console.log("koa server listening on 3000");
+});
+// app.prepare().then(() => {
+//   const server = new Koa();
+//   const router = new Router();
+//   server.use(async (ctx, next) => {
+//     await handle(ctx.req, ctx.res);
+//     ctx.respond = false;
+//   });
+//   server.listen(3000, () => {
+//     console.log("koa server listening on 3000");
+//   });
+// });
