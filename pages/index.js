725c01f07f0c67f1052eba0d57278929b6da5e13
kkxxll <550893685@qq.com>
2021-06-19 12:04:42 +0800

    支持css能力

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,2 @@
+import './test.css'
 export default () => <span>123</span>
--- server.js
+++ server.js
@@ -10,28 +10,28 @@ const handle = app.getRequestHandler();
 const server = new Koa();
 const router = new Router();
 
-router.get('/test', (ctx) => {
-  ctx.body = 'test'
-})
+// router.get('/test', (ctx) => {
+//   ctx.body = 'test'
+// })
 
-server.use(async (ctx, next) => {
-  ctx.body = "INDEX";
-  await next();
-});
-
-server.use(router.routes())
-
-server.listen(3000, () => {
-  console.log("koa server listening on 3000");
-});
-// app.prepare().then(() => {
-//   const server = new Koa();
-//   const router = new Router();
-//   server.use(async (ctx, next) => {
-//     await handle(ctx.req, ctx.res);
-//     ctx.respond = false;
-//   });
-//   server.listen(3000, () => {
-//     console.log("koa server listening on 3000");
-//   });
+// server.use(async (ctx, next) => {
+//   ctx.body = "INDEX";
+//   await next();
 // });
+
+// server.use(router.routes())
+
+// server.listen(3000, () => {
+//   console.log("koa server listening on 3000");
+// });
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+  server.listen(3000, () => {
+    console.log("koa server listening on 3000");
+  });
+});
