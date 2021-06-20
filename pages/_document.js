713bc55678d75c80fa8b667a0b0de5694ac398be
kkxxll <550893685@qq.com>
2021-06-20 12:38:59 +0800

    自定义document

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style>{`.test {color: blue}`}</style>
+        </Head>
+        <body className="test">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
