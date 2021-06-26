40473143256d3d47667b8bf47dff7f9dbd90d80e
kkxxll <550893685@qq.com>
2021-06-26 13:45:38 +0800

    sesstion及数据库存储

--- server.js
+++ server.js
@@ -1,32 +1,47 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const next = require("next");
+const session = require("koa-session");
+const RedisSessionStore = require("./server/session-store");
+const Redis = require("ioredis");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 
 const handle = app.getRequestHandler();
 
-const server = new Koa();
-const router = new Router();
+// 创建redis client
+const redis = new Redis();
 
-// router.get('/test', (ctx) => {
-//   ctx.body = 'test'
-// })
-
-// server.use(async (ctx, next) => {
-//   ctx.body = "INDEX";
-//   await next();
-// });
-
-// server.use(router.routes())
-
-// server.listen(3000, () => {
-//   console.log("koa server listening on 3000");
-// });
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
+
+  server.keys = ["kkxxll for safe"]; // 用于加密的随机字符串
+  const SESSION_CONFIG = {
+    key: "sessionId",
+    store: new RedisSessionStore(redis),
+  };
+
+  server.use(session(SESSION_CONFIG, server));
+
+  server.use(async (ctx, next) => {
+    console.log("user", ctx.session);
+    await next();
+  });
+
+  router.get("/set/user", async (ctx) => {
+    ctx.session.user = {
+      name: "xxx",
+      age: "18",
+    };
+    ctx.body = "set success";
+  });
+
+  router.get("/del/user", async (ctx) => {
+    ctx.session = null;
+    ctx.body = "del success";
+  });
 
   router.get("/a/:id", async (ctx) => {
     const id = ctx.params.id;
@@ -37,7 +52,7 @@ app.prepare().then(() => {
     ctx.respond = false;
   });
 
-  server.use(router.routes())
+  server.use(router.routes());
 
   server.use(async (ctx, next) => {
     await handle(ctx.req, ctx.res);
---
+++ server/session-store.js
@@ -0,0 +1,62 @@
+// const debug = require('debug')('server:session-store')
+
+function debug(err) {
+  console.log(err);
+}
+
+function getRedisId(sid) {
+  return `ssid:${sid}`;
+}
+
+class RedisSessionStore {
+  constructor(client) {
+    this.client = client;
+  }
+
+  async get(sid) {
+    const id = getRedisId(sid);
+    const data = await this.client.get(`${id}`);
+    if (!data) {
+      return null;
+    }
+    try {
+      const result = JSON.parse(data);
+      debug(`get session: ${sid}`, result.githubAuth, result.user.login);
+      return result;
+    } catch (err) {
+      debug("parse session error:", err);
+    }
+  }
+
+  // 当数据设置为非null时，会自动调用set
+  async set(sid, sess, ttl) {
+    const id = getRedisId(sid);
+    if (typeof ttl === "number") {
+      // 转化成秒
+      ttl = Math.ceil(ttl / 1000);
+    }
+    debug(`SET ${sid} ${sess}`);
+    const sessStr = JSON.stringify(sess);
+    if (ttl) {
+      //   debug(
+      //     `SETEX ${sid} ${ttl} ${sess.githubAuth.access_token} ${sess.user.login}`
+      //   );
+      //   setex 表示设置有过期时间的值
+      await this.client.setex(id, ttl, sessStr);
+    } else {
+      //   debug(`SETEX ${sid} ${sess.githubAuth.access_token} ${sess.user.login}`);
+      await this.client.set(id, sessStr);
+    }
+    debug(`SET ${sid} complete`);
+  }
+
+  // 当数据设置为null时候，会自动调用destroy进行清除
+  async destroy(sid) {
+    const id = getRedisId(sid);
+    debug(`DEL ${sid}`);
+    await this.client.del(id);
+    debug(`DEL ${sid} complete`);
+  }
+}
+
+module.exports = RedisSessionStore;
